ecb8e73c527ae2e467c6d144db71f686a779ef09
Mehmet Barış Kalkar <baris.kalkar@opsgenie.com>
2017-11-28 16:22:35 +0300

    Get teams one by one for their team members instead of using expand=member

--- backup-commons/src/main/java/com/opsgenie/tools/backup/retrieval/TeamRetriever.java
+++ backup-commons/src/main/java/com/opsgenie/tools/backup/retrieval/TeamRetriever.java
@@ -2,6 +2,7 @@ package com.opsgenie.tools.backup.retrieval;
 
 import com.opsgenie.oas.sdk.api.TeamApi;
 import com.opsgenie.oas.sdk.api.TeamRoutingRuleApi;
+import com.opsgenie.oas.sdk.model.GetTeamRequest;
 import com.opsgenie.oas.sdk.model.ListTeamRoutingRulesRequest;
 import com.opsgenie.oas.sdk.model.Team;
 import com.opsgenie.oas.sdk.model.TeamRoutingRule;
@@ -10,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
@@ -28,25 +28,40 @@ public class TeamRetriever implements EntityRetriever<TeamConfig> {
     public List<TeamConfig> retrieveEntities() throws InterruptedException {
         logger.info("------------------------------------");
         logger.info("Retrieving current team configurations");
-        final List<Team> teams = teamApi.listTeams(Collections.singletonList("member")).getData();
-        final ConcurrentLinkedQueue<TeamConfig> teamsWithRoutingRules = new ConcurrentLinkedQueue<TeamConfig>();
+        final List<Team> teams = teamApi.listTeams(new ArrayList<String>()).getData();
+        final ConcurrentLinkedQueue<TeamConfig> teamsWithDetails = new ConcurrentLinkedQueue<TeamConfig>();
         ExecutorService pool = Executors.newFixedThreadPool(10);
-        for (final Team team : teams) {
+        for (final Team teamMeta : teams) {
             pool.submit(new Runnable() {
                 @Override
                 public void run() {
                     TeamConfig teamConfig = new TeamConfig();
-                    teamConfig.setTeam(team);
-                    final List<TeamRoutingRule> routingRules = teamRoutingRuleApi.listTeamRoutingRules(new ListTeamRoutingRulesRequest().identifier(team.getId())).getData();
-                    teamConfig.setTeamRoutingRules(routingRules);
-                    teamsWithRoutingRules.add(teamConfig);
+                    boolean failed = false;
+                    try {
+                        final List<TeamRoutingRule> routingRules = teamRoutingRuleApi.listTeamRoutingRules(new ListTeamRoutingRulesRequest().identifier(teamMeta.getId())).getData();
+                        teamConfig.setTeamRoutingRules(routingRules);
+                    } catch (Exception e) {
+                        failed = true;
+                        logger.error("Could not list team routing rules for team: " + teamMeta.getId());
+                    }
+
+                    try {
+                        final Team team = teamApi.getTeam(new GetTeamRequest().identifier(teamMeta.getId())).getData();
+                        teamConfig.setTeam(team);
+                    } catch (Exception e) {
+                        failed = true;
+                        logger.error("Could not get team details for team: " + teamMeta.getId());
+                    }
+                    if (!failed) {
+                        teamsWithDetails.add(teamConfig);
+                    }
                 }
             });
         }
         pool.shutdown();
         while (!pool.awaitTermination(5, TimeUnit.SECONDS)) {
-            logger.info("Populating team routing rules:" + teamsWithRoutingRules.size() + "/" + teams.size());
+            logger.info("Populating team routing rules:" + teamsWithDetails.size() + "/" + teams.size());
         }
-        return new ArrayList<TeamConfig>(teamsWithRoutingRules);
+        return new ArrayList<TeamConfig>(teamsWithDetails);
     }
 }
