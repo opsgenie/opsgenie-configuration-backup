96c81fee88d4ea2ebdad11464fc3a8bf06b99a13
zsengil <zeynep@opsgenie.com>
2017-11-29 17:42:10 +0300

    Import schedule overrides

--- backup-commons/src/main/java/com/opsgenie/tools/backup/retrieval/ScheduleRetriever.java
+++ backup-commons/src/main/java/com/opsgenie/tools/backup/retrieval/ScheduleRetriever.java
@@ -2,7 +2,10 @@ package com.opsgenie.tools.backup.retrieval;
 
 import com.opsgenie.oas.sdk.api.ScheduleApi;
 import com.opsgenie.oas.sdk.api.ScheduleOverrideApi;
+import com.opsgenie.oas.sdk.model.ListScheduleOverrideResponse;
+import com.opsgenie.oas.sdk.model.ListScheduleOverridesRequest;
 import com.opsgenie.oas.sdk.model.Schedule;
+import com.opsgenie.oas.sdk.model.ScheduleOverride;
 import com.opsgenie.tools.backup.dto.ScheduleConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +15,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class ScheduleRetriever implements EntityRetriever<ScheduleConfig> {
+
+    private final boolean includeOverrides;
+
+    public ScheduleRetriever() {
+        includeOverrides = false;
+    }
+
+    public ScheduleRetriever(boolean includeOverrides) {
+        this.includeOverrides = includeOverrides;
+    }
 
     private static final Logger logger = LoggerFactory.getLogger(ScheduleRetriever.class);
 
@@ -28,8 +41,26 @@ public class ScheduleRetriever implements EntityRetriever<ScheduleConfig> {
             ScheduleConfig scheduleConfig = new ScheduleConfig();
             scheduleConfig.setSchedule(schedule);
             scheduleConfigs.add(scheduleConfig);
+            if (includeOverrides) {
+                scheduleConfig.setScheduleOverrideList(listScheduleOverrides(schedule));
+            }
         }
         return scheduleConfigs;
 
+    }
+
+    private List<ScheduleOverride> listScheduleOverrides(Schedule schedule) {
+        try {
+            ListScheduleOverridesRequest listRequest = new ListScheduleOverridesRequest();
+            listRequest.setIdentifier(schedule.getName());
+            listRequest.setScheduleIdentifierType(ListScheduleOverridesRequest.ScheduleIdentifierTypeEnum.NAME);
+            ListScheduleOverrideResponse response = overrideApi.listScheduleOverride(listRequest);
+            if (response != null) {
+                return response.getData();
+            }
+        } catch (Exception e) {
+            logger.error("Could not list schedule overrides for " + schedule.getId());
+        }
+        return Collections.emptyList();
     }
 }
